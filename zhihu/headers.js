2fcfc547dfcfa73de6fc6f212428de21d3b60a22
rocking1212 <rocking1167@foxmail.com>
2019-12-17 10:56:10 +0800

    添加空header文件

---
+++ zhihu/headers.js
@@ -0,0 +1,9 @@
+const cookie = ''
+
+const useragent =
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36'
+
+exports.headers = {
+  Cookie: cookie,
+  'User-Agent': useragent
+}
