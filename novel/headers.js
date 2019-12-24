838dd8e64135e3e4f8424fbf9d766a95517d5c94
rocking1212 <rocking1167@foxmail.com>
2019-12-24 11:01:31 +0800

    2019/12/24

--- novel/headers.js
+++ novel/headers.js
@@ -1,4 +1,5 @@
-const cookie = ''
+const cookie =
+  '_O3-dYNoltm8DV55v5u3n8Q; z_c0="2|1:0|10:1558936960|4:z_c0|92:Mi4xTUE5RUFBQUFBQUFBa0tua0hOeDNEeVlBQUFCZ0FsVk5nTXZZWFFDd3hXcDVEa0c2ZXRVc2l3LWRJeXpzQ0NSTkp3|d6780c5861bbed79291a9ab1928fe73e03e03ba665b0df1a5d687328e79b1cdb"; _xsrf=0aa28ac5-1e7d-44e9-b425-f68619c984f1; __utmc=51854390; __utmv=51854390.100-1|2=registration_date=20140321=1^3=entry_date=20140321=1; q_c1=1308b7181ae3413780f9d7f582abed8a|1562218531000|1558575846000; tst=f; __utma=51854390.1465318953.1560494121.1560494121.1563161389.2; __utmz=51854390.1563161389.2.2.utmcsr=zhihu.com|utmccn=(referral)|utmcmd=referral|utmcct=/follow; tgw_l7_route=060f637cd101836814f6c53316f73463'
 
 const useragent =
   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36'
--- 面经/headers.js
+++ 面经/headers.js
@@ -1,4 +1,4 @@
-const cookie = ''
+const cookie = 'test'
 
 const useragent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36'
 
